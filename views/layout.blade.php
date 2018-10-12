c8e7d17eba3e30da7f747733b9a2fbf995b63871
Mochammad Masbuchin <buchin@dropsugar.com>
2018-10-12 20:24:50 +0700

    https font

--- views/layout.blade.php
+++ views/layout.blade.php
@@ -7,7 +7,7 @@
 
 		<!-- Bootstrap CSS -->
 		<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous">
-		<link href="http://fonts.googleapis.com/css?family=Mr+Dafoe" rel="stylesheet" type="text/css">
+		<link href="https://fonts.googleapis.com/css?family=Mr+Dafoe" rel="stylesheet" type="text/css">
 
 		<style type="text/css">
 			html, body {
