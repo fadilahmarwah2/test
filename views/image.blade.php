a3999d24b91d6c6cab2f9c02e7cc9d03203da3cd
Mochammad Masbuchin <buchin@dropsugar.com>
2018-10-17 15:31:45 +0700

    lanjutan artikel

--- views/image.blade.php
+++ views/image.blade.php
@@ -6,15 +6,22 @@
 
 @section('header')
 <h1>{{ ucwords($keyword) }}</h1>
-<p>
-	<strong>{{ ucfirst($keyword) }}</strong>. {{ collect($sentences)->shuffle()->take(3)->implode(' ') }}
-</p>
+
+@php
+	shuffle($sentences);
+@endphp
+
+<div class="text-center">
+	@if(!empty($sentences))
+		<p>{{ @array_pop($sentences) }} {{ @array_pop($sentences) }}</p>
+	@endif
+</div>
 @endsection
 
 @section('content')
 	
 	@foreach(collect($images)->shuffle()->chunk(3) as $chunked)
-
+		
 		<div class="row">
 			@foreach($chunked as $image)
 				<div class="col-md-4">
@@ -26,15 +33,32 @@
 			
 		</div>
 
-		@if($loop->first)
+		@if($loop->iteration == 2)
+			<div class="row">
+				<div class="col-md-12">
+					<div class="navi text-left">
+						<h3>{{ @array_pop($sentences) }}</h3>
+						<img src="{{ collect($images)->random()['url'] }}" width="34%" align="left" style="margin-right: 8px;margin-bottom: 8px;">
+
+						@foreach(collect($sentences)->chunk(3) as $chunked_sentences)
+							<p>
+								@if($loop->first)
+									<strong>{{ ucfirst($keyword) }}</strong>. 
+								@endif
+
+								@foreach($chunked_sentences as $chunked_sentence)
+									{{ $chunked_sentence }} 
+								@endforeach
+							</p>
+						@endforeach
+					</div>
+				</div>
+			</div>
 
 			<div class="row">
 				<div class="col-md-12">
 					<div class="navi text-center bg-dark text-white">
-						<p>{{ collect($sentences)->shuffle()->take(3)->implode(' ') }}</p>
-						<p>
-							You might also like:
-						</p>
+						<p>{{ @array_pop($sentences) }}</p>
 						@foreach($related as $r)
 							@if(data_exists($r))
 								<a class="badge badge-{{ collect(['primary', 'secondary', 'success', 'info', 'danger', 'warning', 'light', 'dark'])->random() }}" href="{{ image_url($r) }}">{{ $r }}</a>
@@ -43,9 +67,12 @@
 					</div>
 				</div>
 			</div>
+			
 		@endif
 
 	@endforeach
+	
+
 
 	
 	
